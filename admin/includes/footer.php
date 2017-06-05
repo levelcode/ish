659cab93eec34562343356705c12b91cceba7cb3
Michael Alfonso <mac@iMac-de-Mac-2.local>
2017-06-05 17:25:55 -0500

    add admin

---
+++ admin/includes/footer.php
@@ -0,0 +1,14 @@
+<div id="footer" class="container">
+  <div class="row">
+    <div class="col col-xs-12 col-md-7 col-lg-7 col-lg-offset-3 col-md-offset-3">
+      <div class="title col-xs-9 col-md-9 col-lg-9">
+        <strong>La Subasta de Papá</strong> 2017
+      </div>
+      <div class="img col-xs-3 col-md-3 col-lg-3">
+        <a href="#">
+          <img src="../assets/logo_ishop.png" alt="Ishop" title="Ishop">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>La subasta de papá</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/styles.min.css">
+
+  </head>
+  <body>
+
+    <div class="container">
+      <?php include "./includes/header.php" ?>
+
+      <?php include "./includes/banner.php" ?>
+      <div id="main">
+        <div class="main">
+          <?php include "./includes/steps.php" ?>
+          <?php include "./includes/form.php" ?>
+          <?php include "./includes/gallery.php" ?>
+        </div>
+      </div>
+
+      <?php include "./includes/footer.php" ?>
+    </div>
+    <script src="js/vendors.min.js"></script>
+    <script src="js/scripts.min.js"></script>
+
+  </body>
+</html>
