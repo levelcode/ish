a95bdae035fa7e210301b5f7ba5fbbda19933b8f
Michael Alfonso <mac@iMac-de-Mac-2.local>
2017-06-05 17:33:35 -0500

    add snippet code FB

--- index.php
+++ index.php
@@ -11,6 +11,23 @@
   <body>
 
     <div class="container">
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '1320185844755160',
+            xfbml      : true,
+            version    : 'v2.9'
+          });
+          FB.AppEvents.logPageView();
+        };
+        (function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "//connect.facebook.net/en_US/sdk.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));
+      </script>
       <?php include "./includes/header.php" ?>
 
       <?php include "./includes/banner.php" ?>
