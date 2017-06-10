404cb646df9fc72df5305ca58c09576281666a21
Leonardo Palomino <leonardo.palomino@gmail.com>
2017-06-10 02:09:56 -0500

    Ajustes

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="css/styles.min.css">
+    <meta property="og:url"         content="https://lasubastadepapa.com" />
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content="La Subasta de Papa" />
+    <meta property="og:description"        content="Compra el regalo de papá en iShop y por tus compras de $1´000.000 o más, participa y gana una Juguetería completa para papá, que incluye: iPhone 7, MacBook Air, Apple TV, Audífonos Beats y iPad" />
+    <meta property="og:image"              content="https://lasubastadepapa.com/assets/share_fb.jpg" />
 
   </head>
   <body class="<?php (isset($_SESSION['fb_user']))?print'user-login':print'user-logout'; ?>">
